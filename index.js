c8eeacd8b70f4d3aa40b432d0ac518611cd69886
Franceso Toscano <ftoscano.ch@gmail.co>
2019-10-25 23:45:28 +0200

    formattazione

--- index.js
+++ index.js
@@ -3,18 +3,9 @@ const { VERSIONS } = constants;
 
 let config = {
 	profiles: {
-		patient: {
-			service: './services/patient.service.js',
-			versions: [VERSIONS['4_0_0']]
-		},
-		encounter: {
-			service: './services/encounter.service.js',
-			versions: [VERSIONS['4_0_0']]
-		},
-		bundle: {
-			service: './services/bundle.service.js',
-			versions: [VERSIONS['4_0_0']]
-		}
+		patient: { service: './services/patient.service.js', versions: [VERSIONS['4_0_0']] },
+		encounter: { service: './services/encounter.service.js', versions: [VERSIONS['4_0_0']] },
+		bundle: { service: './services/bundle.service.js', versions: [VERSIONS['4_0_0']] }
 	}
 };
 let server = initialize(config);
