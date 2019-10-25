09230ce3867bc57cf19c29148f3c6df330095cb6
Franceso Toscano <ftoscano.ch@gmail.co>
2019-10-25 18:51:36 +0200

    aggiunto attributi a Encounter

--- services/encounter.service.js
+++ services/encounter.service.js
@@ -7,8 +7,10 @@ let Coding = undefined;
 module.exports.search = (args) => new Promise((resolve, reject) => {
     logger.info('encounter.search');
     const { base_version, _id } = args;
-    resolve(findAll(base_version));
+    resolve(_id ? findById(base_version, _id) : findAll(base_version));
 });
+
+const findById = (base_version, id) => findAll(base_version).find((p) => p.id == id);
 
 const findAll = (base_version) => {
     if (encounterDB.length == 0) {
@@ -28,14 +30,18 @@ const create = (base_version, id) => {
     }
 
     const e = new Encounter();
-    e.id = 'encunter-' + id;
+    e.id = 'encounter-' + id;
     e.status = 'finished';
     e.class = new Coding();
     e.class.system = 'http://terminology.hl7.org/CodeSystem/v3-ActCode';
     e.class.code = 'AMB';
     e.class.display = 'ambulatory';
-    e.subject = {reference: 'Patient/patient-5'};
-    e.participant = [];
+    e.subject = {reference: 'Patient/patient-5', display: 'Personcina Malatina'};
+    e.participant = [{individual: {reference: 'Practitioner/xpln-123456', display: 'Prof. Dr. med. Medicone Professorone'}}];
     e.period = {start: "2015-01-17", end: "2015-01-17"};
+    e.appointment = {reference: 'Appointment/appointment-' + id, display: 'Appuntamento Follow up 3 mesi'};
+    e.episodeOfCare = {reference: 'EpisodeOfCare/labelnumber-' + id};
+    e.location = [{location: {reference: 'Location/areaGuid-' + id, display:'Ambulatorio di Fuffologia' }}];
+    e.diagnosis = [{condition: {display:'diagnosi 1'}}, {condition: {display:'diagnosi 2'}}];
     return e;
 }
