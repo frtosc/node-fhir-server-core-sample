48330d80713adf943434bd42f9c38af2e2ae449a
Franceso Toscano <ftoscano.ch@gmail.co>
2019-10-25 22:24:01 +0200

    rank diagnosis, reasonCode

--- services/encounter.service.js
+++ services/encounter.service.js
@@ -2,7 +2,6 @@ const { resolveSchema, loggers} = require('@asymmetrik/node-fhir-server-core');
 const logger = loggers.get('default');
 const encounterDB = [];
 let Encounter = undefined;
-let Coding = undefined;
 
 module.exports.search = (args) => new Promise((resolve, reject) => {
     logger.info('encounter.search');
@@ -25,23 +24,21 @@ const create = (base_version, id) => {
     if (Encounter === undefined) {
         Encounter = require(resolveSchema(base_version, 'Encounter'));
     }
-    if (Coding === undefined) {
-        Coding = require(resolveSchema(base_version, 'Coding'))
-    }
 
     const e = new Encounter();
     e.id = 'encounter-' + id;
     e.status = 'finished';
-    e.class = new Coding();
-    e.class.system = 'http://terminology.hl7.org/CodeSystem/v3-ActCode';
-    e.class.code = 'AMB';
-    e.class.display = 'ambulatory';
+    e.class = {system: 'http://terminology.hl7.org/CodeSystem/v3-ActCode', code: 'AMB', display: 'ambulatory'};
     e.subject = {reference: 'Patient/patient-5', display: 'Personcina Malatina'};
-    e.participant = [{individual: {reference: 'Practitioner/xpln-123456', display: 'Prof. Dr. med. Medicone Professorone'}}];
+    e.participant = [
+        {individual: {reference: 'Practitioner/xpln-123456', display: 'Prof. Dr. med. Medicone Professorone'}},
+        {individual: {reference: 'Practitioner/xpln-789012', display: 'Infermierino Inoino'}}
+    ];
     e.period = {start: "2015-01-17", end: "2015-01-17"};
     e.appointment = {reference: 'Appointment/appointment-' + id, display: 'Appuntamento Follow up 3 mesi'};
     e.episodeOfCare = {reference: 'EpisodeOfCare/labelnumber-' + id};
     e.location = [{location: {reference: 'Location/areaGuid-' + id, display:'Ambulatorio di Fuffologia' }}];
-    e.diagnosis = [{condition: {display:'diagnosi 1'}}, {condition: {display:'diagnosi 2'}}];
+    e.reasonCode = {text: 'Motivo della visita'};
+    e.diagnosis = [{condition: {display:'diagnosi 2'}, rank: 1}, {condition: {display:'diagnosi 2'}, rank: 2}];
     return e;
 }
