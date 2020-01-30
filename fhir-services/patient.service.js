6dde590e31a60894ac863b3de888dade6375b00a
edptof <francesco.toscano@eoc.ch>
2020-01-30 17:37:44 +0100

    integrazione servizi feathers con fhir

--- fhir-services/patient.service.js
+++ fhir-services/patient.service.js
@@ -1,28 +1,28 @@
-const { resolveSchema, loggers } = require('@asymmetrik/node-fhir-server-core');
-const logger = loggers.get('default');
-const getPatient = (base_version) => require(resolveSchema(base_version, 'Patient'));
-const patientsDB = [];
+const { resolveSchema, loggers } = require("@asymmetrik/node-fhir-server-core");
+const logger = loggers.get("default");
+const getPatient = base_version =>
+  require(resolveSchema(base_version, "Patient"));
 
-module.exports.search = (args) => new Promise((resolve, reject) => {
-    logger.info('patient.search');
+module.exports.search = args =>
+  new Promise((resolve, reject) => {
+    logger.info("patient.search");
     const { base_version, _id } = args;
-    resolve(_id ? findById(base_version, _id) : findAll(base_version));
-});
 
-module.exports.searchById = (args) => new Promise((resolve, reject) => {
-    logger.info('patient.searcById');
+    if (_id) resolve(createFhirPatient(base_version, _id));
+
+    this.app.service("/lista-neoc").find().then(neocs =>
+      resolve(neocs.map(neoc => createFhirPatient(base_version, neoc)))
+    );
+  });
+
+module.exports.searchById = args =>
+  new Promise((resolve, reject) => {
+    logger.info("patient.searcById");
     let { base_version, id } = args;
-    resolve(findById(base_version, id));
-});
+    resolve(resolve(createFhirPatient(base_version, id)));
+  });
 
-const findById = (base_version, id) => findAll(base_version).find((p) => p.id == id);
-
-const findAll = (base_version) => {
-    if (patientsDB.length == 0) {
-        const Patient = getPatient(base_version)
-        for (let i = 0; i < 20; i++) {
-            patientsDB.push(new Patient({ id: i }));
-        }
-    }
-    return patientsDB;
-}
+const createFhirPatient = (base_version, neoc) => {
+  const Patient = getPatient(base_version);
+  return new Patient({ id: neoc });
+};
--- index.js
+++ index.js
@@ -1,36 +1,55 @@
-const feathers = require('@feathersjs/feathers');
-const express = require('@feathersjs/express');
-const { initialize, loggers, constants } = require('@asymmetrik/node-fhir-server-core');
+const feathers = require("@feathersjs/feathers");
+const express = require("@feathersjs/express");
+const {
+  initialize,
+  loggers,
+  constants
+} = require("@asymmetrik/node-fhir-server-core");
 const { VERSIONS } = constants;
 
-const logger = loggers.get('default');
+const logger = loggers.get("default");
 
 const app = express(feathers());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.configure(express.rest());
 app.use(express.errorHandler());
-app.use('/feathers', {
-	async find(params) {
-		logger.info('feathers ', params.query);
-		return params;
-	}
+app.use("/lista-neoc", {
+  async find() {
+    logger.info("/lista-neoc ");
+    return [1234567, 9999999, 7777777, 5556667];
+  }
 });
-app.get('/fhir/*', (req, res) => {
-	res.redirect(req.path.substr(5))
+
+app.get("/fhir/*", (req, res) => {
+  res.redirect(req.path.substr(5));
 });
 
 const config = {
-	profiles: {
-		patient: { service: './fhir-services/patient.service.js', versions: [VERSIONS['4_0_0']] },
-		encounter: { service: './fhir-services/encounter.service.js', versions: [VERSIONS['4_0_0']] },
-		bundle: { service: './fhir-services/bundle.service.js', versions: [VERSIONS['4_0_0']] },
-		structureDefinition: { service: './fhir-services/structure-definition.service.js', versions: [VERSIONS['4_0_0']] }
-	}
+  profiles: {
+    patient: {
+      service: "./fhir-services/patient.service.js",
+      versions: [VERSIONS["4_0_0"]]
+    },
+    encounter: {
+      service: "./fhir-services/encounter.service.js",
+      versions: [VERSIONS["4_0_0"]]
+    },
+    bundle: {
+      service: "./fhir-services/bundle.service.js",
+      versions: [VERSIONS["4_0_0"]]
+    },
+    structureDefinition: {
+      service: "./fhir-services/structure-definition.service.js",
+      versions: [VERSIONS["4_0_0"]]
+    }
+  }
 };
 
 initialize(config, app);
 
-app.listen(3000).on('listening', () =>
-	logger.info('Starting the FHIR Server at localhost:3000')
-);
+require("./fhir-services/patient.service.js").app = app;
+
+app.listen(3000).on("listening", () => {
+  logger.info("Starting the FHIR Server at localhost:3000");
+});
